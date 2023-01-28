0e86da4fac32d6ae9a57d09234e9b8872af1aad3
Doltons Edward Nicholas <edwarddoltons@gmail.com>
2023-01-28 22:31:49 +0800

    feat: setup data loader

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,13 @@
+import { useLoaderData } from "@remix-run/react";
+import supabase from "utils/supabase";
+
+export const loader = async () => {
+  const { data } = await supabase.from("messages").select();
+  return { data };
+};
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  const { data } = useLoaderData();
+
+  return <div>{JSON.stringify(data)}</div>;
 }
